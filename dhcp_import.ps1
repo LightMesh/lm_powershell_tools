9afe3d0869693e1424d6e9f1b504827ad2671d88
Dimiter Todorov <dimiter.todorov@gmail.com>
2015-08-19 16:06:23 +0000

    Add XN API Cmdlets Update LM DHCP Import

--- dhcp_import.ps1
+++ dhcp_import.ps1
@@ -1,4 +1,5 @@
-﻿<#
+﻿function LM-DHCP-Import{
+<#
 .SYNOPSIS 
 Extracts DHCP Server information into JSON Format and optionally sends it to LightMesh
 
@@ -6,21 +7,12 @@ Extracts DHCP Server information into JSON Format and optionally sends it to Lig
 The script works by reading through the DHCP server objects for Scopes, Reservations, and Leases.
 The information is stored in custom objects which are then serialized to JSON.
 Optionally the JSON Output can be sent to directly to the LightMesh host for processing.
+
+.PARAMETER ExportFile
+Optional path to a file where to write the JSON output. (e.g. C:\lm_dhcp_scopes.json)
     
 .PARAMETER LMImport
 Switch defining whether to send the data to LightMesh
-
-.PARAMETER LMHost
-Specifies the hostname or ip of the LightMesh appliance.
-
-.PARAMETER LMPort
-Specifies the port to use for connecting to the LightMesh appliance.
-
-.PARAMETER LMSecure
-Specifies whether the connection is performed over SSL. If using HTTPS, the certificate should be trusted.
-
-.PARAMETER LMToken
-Specifies the application token to use when connecting to LightMesh. Should be in the format of "application_id token"
 
 .INPUTS
 None. You cannot pipe objects to the script.
@@ -29,11 +21,12 @@ None. You cannot pipe objects to the script.
 None. 
 
 .EXAMPLE
-C:\PS> .dhc
+C:\PS> LM-DHCP-Import
 #>
 
 [CmdletBinding()]
 Param(
+   [string]$ExportFile,
    [switch]$LMImport,
    [string]$LMHost,
    [int]$LMPort,
@@ -78,7 +71,7 @@ foreach($scope in $scopes){
         Add-Member -InputObject $lease_object -MemberType NoteProperty -Name ip -Value $lease.IpAddress.IPAddressToString
         Add-Member -InputObject $lease_object -MemberType NoteProperty -Name hostname -Value $lease.IpAddress.HostName
         if($lease.LeaseExpiryTime){
-            $lease_expiry=[Math]::Floor([decimal](Get-Date($lease.LeaseExpiryTime).ToUniversalTime()-uformat "%s"))
+            $lease_expiry=(Get-Date(Get-Date($lease.LeaseExpiryTime).ToUniversalTime()) -format s) + "Z"
         }else{
             $lease_expiry=$null
         }
@@ -92,10 +85,30 @@ foreach($scope in $scopes){
 }
 
 Add-Member -InputObject $dhcp_server_object -MemberType NoteProperty -Name hostname -Value $hostname
-Add-Member -InputObject $dhcp_server_object -MemberType NoteProperty -Name scopes -Value $dhcp_scopes
+Add-Member -InputObject $dhcp_server_object -MemberType NoteProperty -Name dhcp_scopes -Value $dhcp_scopes
 
 
 $dhcp_json_object = $dhcp_server_object | ConvertTo-Json -Depth 5
 
-
-Write-Verbose $dhcp_json_object
+If ($ExportFile){
+    $dhcp_json_object | Out-File -FilePath $ExportFile
+}
+If ($LMImport){
+    Write-Host "Proceeding with Import"
+    $existing_dhcp_object=XN Get "/is/dhcp_server/filter/name?name=$hostname" -Silent
+    if($existing_dhcp_object){
+        Write-Host "Existing DHCP Server found at" $existing_dhcp_object.meta.xnid
+        $dhcp_server_record=$existing_dhcp_object
+    }else{
+        Write-Host "No DHCP Server record foound for $hostname in LM. Creating record."
+        $obj=New-Object PSObject -Property @{
+        "name"=$hostname
+        }
+        $dhcp_server_record = XN Put "/model/dhcp_server" ($obj | ConvertTo-Json) -Silent
+        Write-Host "Created DHCP Server record for $hostname with XNID " $dhcp_server_record.meta.xnid 
+    }
+    $action_path=$dhcp_server_record.meta.xnid + "/action/update_all_scopes"
+    $result=XN POST $action_path $dhcp_json_object -Silent
+    return $result
+}
+}
---
+++ xn_api.ps1
@@ -0,0 +1,155 @@
+﻿#XN API Cmdlets for Powershell
+
+function XN-Login{
+<#
+.SYNOPSIS 
+Log into an XN API Endpoint.
+
+.DESCRIPTION
+This Cmdlet will log you in and create a token for use during your session. This is stored in $env:XN_TOKEN
+
+.PARAMETER base_url
+Full URL to the XN Instance. e.g. http://applianceserver.lightmesh.com
+    
+.PARAMETER username
+Username/Email with which to log in.
+
+.PARAMETER password
+Password
+
+.INPUTS
+None. You cannot pipe objects to the script.
+
+.OUTPUTS
+None. 
+
+.EXAMPLE
+C:\PS> XN-Login -base_url http://applianceserver.lightmesh.com -username "someuser@mydomain.com" -password "SecretSquirrel"
+#>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$true)]
+        [string]$base_url,
+
+        [Parameter(Mandatory=$true)]
+        [string]$username,
+
+        [Parameter(Mandatory=$true)]
+        [string]$password
+
+    )
+    $user=New-Object PSObject -Property @{
+        "email"=$username
+        "password"=$password
+    }
+    $post_data=New-Object PSObject -Property @{
+        "user"=$user
+    }
+    $post_json=ConvertTo-Json $post_data
+
+    $token = Invoke-RestMethod -Uri $base_url"/sessions.json" -Method Post -ContentType "application/json" -Body $post_json
+    
+    If($token){
+        Write-Host "Logged Into $base_url as $username. XN_URL and XN_TOKEN have been exported to the environment"
+    }
+    $env:XN_URL=$base_url
+    $env:XN_TOKEN=$token.token
+    return $null
+}
+
+function XN-Logout{
+<#
+.SYNOPSIS 
+Log out of XN
+
+.DESCRIPTION
+This Cmdlet will log you out and clear the environment variables for XN_URL and XN_TOKEN
+
+.INPUTS
+None. You cannot pipe objects to the script.
+
+.OUTPUTS
+None. 
+
+.EXAMPLE
+C:\PS> XN-Logout
+#>
+    If (($env:XN_URL) -and ($env:XN_TOKEN)){
+        XN Delete "/account"
+    }
+    $env:XN_URL=$null
+    $env:XN_TOKEN=$null
+    Write-Host "Successfully Logged Out"
+}
+
+function XN{
+<#
+.SYNOPSIS 
+Perform REST Actions against an XN Server
+
+.DESCRIPTION
+This Cmdlet allows you to perform GET, POST PATCH, PUT, DELETE Requests against an XN Endpoint
+
+.PARAMETER method
+Can be one of GET, POST PATCH, PUT, DELETE
+    
+.PARAMETER path
+URL Endpoint. e.g. "/is/subnet"
+
+.PARAMETER body
+Optional. This should be a JSON-formatted string.
+
+.PARAMETER Silent
+Optional. If this flag is set the JSON object is not printed out to screen. 
+
+.INPUTS
+None. You cannot pipe objects to the script.
+
+.OUTPUTS
+Powershell Object matching the JSON returned from XN. 
+
+.EXAMPLE
+PS C:\> $subnet=XN GET '/is/subnet/first'
+
+.EXAMPLE
+PS C:\> (XN GET '/is/subnet/first' -Silent).meta.xnid
+/model/subnet/id/550
+
+#>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$true, Position=1)]
+        [ValidateSet("Get","Post","Patch","Put","Delete")]
+        [string]$method,
+
+        [Parameter(Mandatory=$true, Position=2)]
+        [string]$path,
+
+        [Parameter(Position=3)]
+        [string]$body,
+
+        [Parameter()]
+        [switch]$Silent
+    )
+    $xn_version="/v1"
+    If(($env:XN_URL) -and ($env:XN_TOKEN)){
+        $headers=@{"AUTHORIZATION"=$env:XN_TOKEN; "accept"="application/json, text/javascript"}
+        $action_method=$method.ToString().ToUpper()
+        switch ($action_method){
+            "GET"{$output=Invoke-RestMethod -Uri $env:XN_URL$xn_version$path -Method Get -ContentType "application/json" -Headers $headers}
+            "POST"{$output=Invoke-RestMethod -Uri $env:XN_URL$xn_version$path -Method Post -ContentType "application/json" -Headers $headers -Body $body}
+            "PATCH"{$output=Invoke-RestMethod -Uri $env:XN_URL$xn_version$path -Method Patch -ContentType "application/json" -Headers $headers -Body $body}
+            "PUT"{$output=Invoke-RestMethod -Uri $env:XN_URL$xn_version$path -Method Put -ContentType "application/json" -Headers $headers -Body $body}
+            "DELETE"{$output=Invoke-RestMethod -Uri $env:XN_URL$xn_version$path -Method Delete -ContentType "application/json" -Headers $headers}
+        }
+        
+        If(-Not($silent)){
+            $json_output=$output | ConvertTo-Json -Depth 100
+            Write-Host $json_output
+        }
+        return $output
+    }else{
+        throw "XN_URL and XN_TOKEN not found in environment. Please use XN-Login to log in."
+    }
+
+}
