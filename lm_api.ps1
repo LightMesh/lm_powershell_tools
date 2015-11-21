8b0b0b5eed602e65eec2c256b784a4743567ad56
Dimiter Todorov <dimiter.todorov@gmail.com>
2015-11-20 23:21:19 -0500

    LM Api DEV

---
+++ lm_api.ps1
@@ -0,0 +1,17 @@
+﻿function LM-Add-Note{
+#BETA
+Param (
+        [Parameter(Mandatory=$true, Position=1)]
+        [int]$id,
+
+        [Parameter(Mandatory=$true, Position=2)]
+        [string]$text
+    )
+$note = New-Object -TypeName PSObject
+$has_notes = New-Object -TypeName PSObject
+$has_notes | Add-Member -MemberType NoteProperty -Name set $id
+$note | Add-Member -MemberType NoteProperty -Name has_notes $has_notes
+$json=$note  | ConvertTo-Json
+Write-Host $json
+ XN PUT "/model/note?format=full,properties" $json
+}
